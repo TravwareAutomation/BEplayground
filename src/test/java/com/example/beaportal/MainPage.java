7795d8d33b0478221ab801fe3a948ef4fcfb2d10
TravwareAutomation <hassan.balaha@travware.com>
2024-12-29 17:05:01 +0200

    commit regarding BE first automation

--- src/test/java/com/example/beaportal/MainPage.java
+++ src/test/java/com/example/beaportal/MainPage.java
@@ -7,9 +7,9 @@ import org.openqa.selenium.support.PageFactory;
 // page_url = https://www.jetbrains.com/
 public class MainPage {
 
-    public String site="https://b2c.travware.com/"
-
-    @FindBy(xpath = "/html/body/div[1]/main/div/div[1]/div/div/div[2]/div/div[1]/details[1]/summary']")
+    public String site="https://b2c.travware.com/";
+// flight passenger and class details
+    @FindBy(xpath = "/html/body/div[1]/main/div/div[1]/div/div/div[2]/div/div[1]/details[1]/summary")
     public WebElement flightTypeSelector;
         @FindBy(xpath = "/html/body/div[1]/main/div/div[1]/div/div/div[2]/div/div[1]/details[1]/ul/li[3]")
         public WebElement FlightType;
@@ -21,7 +21,24 @@ public class MainPage {
     @FindBy(xpath = "/html/body/div[1]/main/div/div[1]/div/div/div[2]/div/div[1]/details[3]/summary")
     public WebElement ClassSelector;
         @FindBy(xpath = "/html/body/div[1]/main/div/div[1]/div/div/div[2]/div/div[1]/details[3]/ul/li[1]/a")
-        public WebElement NumberOfPassengers;
+        public WebElement classType;
+
+        //asserting a roundtrip is in order by checking the "Returning date is available"
+
+    @FindBy(xpath = "/html/body/div[1]/main/div/div[1]/div/div/div[2]/div/form/span/div[3]/label/span")
+    public WebElement returnDate;
+// flight city and date details
+    @FindBy(xpath = "/html/body/div[1]/main/div/div[1]/div/div/div[2]/div/form/span/div[1]/div[1]/input")
+    public WebElement departingCit;
+        @FindBy(xpath = "/html/body/div[1]/main/div/div[1]/div/div/div[2]/div/form/span/div[1]/div[1]/ul")
+        public  WebElement departingCityCairo;
+    @FindBy(xpath = "/html/body/div[1]/main/div/div[1]/div/div/div[2]/div/form/span/div[1]/div[2]/input")
+    public WebElement arrivalCity;
+        @FindBy(xpath = "/html/body/div[1]/main/div/div[1]/div/div/div[2]/div/form/span/div[1]/div[2]/ul/li/ul/li/a/div/div/h3")
+        public  WebElement arrivalCityDubai;
+    @FindBy(xpath = "/html/body/div/main/div/div[1]/div/div/div[2]/div/form/span/button")
+    public WebElement SearchButton;
+
 
         public MainPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
--- src/test/java/com/example/beaportal/MainPageTest.java
+++ src/test/java/com/example/beaportal/MainPageTest.java
@@ -1,60 +1,94 @@
 package com.example.beaportal;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.*;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
+import java.util.List;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
 public class MainPageTest {
     private WebDriver driver;
+
     private MainPage mainPage;
 
         @BeforeMethod    public void setUp() {
         ChromeOptions options = new ChromeOptions();
-        mainPage = new MainPage(driver);
         // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-                driver.get(mainPage.);
+        mainPage = new MainPage(driver);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(mainPage.site);
 
     }
 
-        @AfterMethod    public void tearDown() {
-        driver.quit();
+//        @AfterMethod    public void tearDown() {driver.quit();}
+
+    @Test
+    public void testingFlightCaiDxb() throws InterruptedException {
+        Wait<WebDriver>wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        //selecting flight type (oneway , round trip , multi cities)
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        mainPage.flightTypeSelector.click();
+//        mainPage.FlightType.click();
+        Select dropdown;
+        //selecting passenger number
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        mainPage.NumberOfPassengersSelector.click();
+        mainPage.NumberOfPassengers.click();
+        mainPage.NumberOfPassengersSelector.click();
+
+        //selecting class type (economy , first , business)
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        mainPage.ClassSelector.click();
+        mainPage.classType.click();
+
+        assertTrue(mainPage.returnDate.isDisplayed());
+        // entring flight details (cities )
+        //choosing depart city
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        mainPage.departingCit.sendKeys("Cairo");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        List<WebElement> DepartCityList=driver.findElements(By.xpath("/html/body/div[1]/main/div/div[1]/div/div/div[2]/div/form/span/div[1]/div[1]/ul"));
+        DepartCityList.get(0).findElement(By.partialLinkText("Cairo")).click();
+        //choosing arrival city
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        mainPage.arrivalCity.sendKeys("dxb");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        assertTrue(mainPage.arrivalCityDubai.isDisplayed());
+        mainPage.arrivalCityDubai.click();
+        mainPage.SearchButton.click();
+
+
+
     }
+//
+//        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test='search-input']"));
+//                assertEquals(searchPageField.getAttribute("value"), "Selenium");    }
 
-    @Test
-    public void search() {
-        mainPage.searchButton.click();
-
-        WebElement searchField = driver.findElement(By.cssSelector("[data-test='search-input']"));
-        searchField.sendKeys("Selenium");
-
-        WebElement submitButton = driver.findElement(By.cssSelector("button[data-test='full-search-button']"));
-        submitButton.click();
-
-        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test='search-input']"));
-                assertEquals(searchPageField.getAttribute("value"), "Selenium");    }
-
-    @Test
-    public void toolsMenu() {
-        mainPage.toolsMenu.click();
-
-        WebElement menuPopup = driver.findElement(By.cssSelector("div[data-test='main-submenu']"));
-        assertTrue(menuPopup.isDisplayed());
-    }
-
-    @Test
-    public void navigationToAllTools() {
-        mainPage.seeDeveloperToolsButton.click();
-        mainPage.findYourToolsButton.click();
-
-        WebElement productsList = driver.findElement(By.id("products-page"));
-        assertTrue(productsList.isDisplayed());
-                assertEquals(driver.getTitle(), "All Developer Tools and Products by JetBrains");    }
+//    @Test
+//    public void toolsMenu() {
+//        mainPage.toolsMenu.click();
+//
+//        WebElement menuPopup = driver.findElement(By.cssSelector("div[data-test='main-submenu']"));
+//        assertTrue(menuPopup.isDisplayed());
+//    }
+//
+//    @Test
+//    public void navigationToAllTools() {
+//        mainPage.seeDeveloperToolsButton.click();
+//        mainPage.findYourToolsButton.click();
+//
+//        WebElement productsList = driver.findElement(By.id("products-page"));
+//        assertTrue(productsList.isDisplayed());
+//                assertEquals(driver.getTitle(), "All Developer Tools and Products by JetBrains");    }
 }
